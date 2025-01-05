233a2d2ca6e2009dbb7ebdb985c01d200ae53373
dolores3 <dolores@atom.nl>
2025-01-05 23:25:12 +0100

    shouldve stopped this when i had the chance

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //dd('test');
+        if (auth()->check() && auth()->user()->admin === 1) {
+            return $next($request);
+        }
+        abort(403, 'Unauthorized action.');
+        }
+}
